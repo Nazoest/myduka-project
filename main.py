d962628b68b2ea9a95e652a71b07f7b7249ec941
Nathan <nathanmacharia115@gmail.com>
2025-10-30 14:45:32 +0300

    stock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect,render_template, request, url_for
-from database import fetch_data, insert_products
+from database import fetch_data, insert_products,insert_sales,insert_stock
 
 #instance of flask class
 app=Flask(__name__)
@@ -25,17 +25,38 @@ def add_products():
 
     return redirect(url_for('prods'))
 
+@app.route('/add_sales',methods=['GET','POST'])
+def add_sales():
+    if request.method=='POST':
+        pid=request.form['pid']
+        quantity=request.form["quantity"]
+        new_sale = (pid,quantity)
+        print(new_sale)
+        insert_sales(new_sale)
+
+    return redirect(url_for('sales'))
+
+@app.route('/add_stock',methods=['GET','POST'])
+def add_stock():
+    if request.method=='POST':
+        pid=request.form['pid']
+        quantity=request.form["stock_quantity"]
+        new_stock = (pid,quantity)
+        insert_stock(new_stock)
+
+    return redirect(url_for('stock'))
+
 
 @app.route('/sales')
 def sales():
     sales=fetch_data('sales')
-    print(sales)
-    return render_template('sales.html',sales=sales)
+    products=fetch_data('products')
+    return render_template('sales.html',sales=sales,products=products)
 
 @app.route('/stock')
 def stock():
     stock=fetch_data('stock')
-    print(stock)
-    return render_template('stock.html',stock=stock)
+    products=fetch_data('products')
+    return render_template('stock.html',stock=stock,products=products)
 
 app.run(debug=True)
